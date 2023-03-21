91e9ff4f75a1e254228e250a7d4a918ce282c6d5
Roberts82 <robert.auzins@gmail.com>
2023-03-21 23:20:03 +0000

    Improved README file some technical improvements in the script.

--- 01_script_BlacklistDataScr.R
+++ 01_script_BlacklistDataScr.R
@@ -21,7 +21,7 @@ library(RColorBrewer)
 # 10) 20.12.2022. "Grozījums Ministru kabineta 2020. gada 17. decembra noteikumos Nr. 819 "Noteikumi par zemu nodokļu vai beznodokļu valstīm un teritorijām"" https://m.likumi.lv/doc.php?id=338298
 
 
-
+# Scraping data from Regulations and creating dataframe width 2 columns (1) No of point in the Regulation; 2) name of jurisdiction)
 
 # ***** I. YEAR 1995 VERSION *****
 # Define the URL to scrape
@@ -57,6 +57,7 @@ view(valstis95)
 
 
 # ***** II. YEAR 1997 VERSION (AMENDMENTS 1 TO YEAR 95 VERSION) *****
+# (excluding from the list a jurisdiction that was excluded form the blacklist)
 
 valstis97 <- valstis95 %>% 
   filter(pkt.95 != "48.")
@@ -239,7 +240,7 @@ valstis2022_2 <- str_split_fixed(valstis2022_2, " ", 2)
 valstis2022_2 <- `colnames<-`(valstis2022_2, c("pkt.2020", "teritorija"))
 valstis2022_2 <- as.data.frame(valstis2022_2)
 
-# remove the unnecessary symbols at the end of strings
+# remove the unnecessary symbols at the end of strings (this will help to find unique variables and join data width other dataframes later on)
 valstis2022_2$teritorija <- str_remove_all(valstis2022_2$teritorija, ";")
 valstis2022_2$teritorija <- str_remove_all(valstis2022_2$teritorija, ".\"")
 
@@ -279,30 +280,32 @@ df2 <- ter_unik
 # vec2 <- as.vector(df2)
 # amatch(vec1, vec2, maxDist = 10)
 
+# this code seems will not be used in the project (the idea was to classify jurisdictions
+# and find duplicated, however other methods are expected to be more efficient):
 # stringdistmatrix(df1$teritorija, df2$teritorija, useNames = TRUE)
-p <- stringdistmatrix(df1$teritorija, df2$teritorija, useNames = TRUE) # uses default method "lv"
-p_osa <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "osa", useNames = TRUE)
-p_dl <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "dl", useNames = TRUE)
-p_hamming <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "hamming", useNames = TRUE)
-p_lcs <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "lcs", useNames = TRUE)
-p_qgram <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "qgram", useNames = TRUE)
-p_cosine <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "cosine", useNames = TRUE)
-p_jaccard <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "jaccard", useNames = TRUE)
-p_jw <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "jw", useNames = TRUE)
-p_soundex <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "soundex", useNames = TRUE)
-
-heatmap(p, col = cm.colors(256))
-heatmap(p_osa, col = cm.colors(256))
-heatmap(p_dl, col = cm.colors(256))
-heatmap(p_hamming, col = cm.colors(256))
-heatmap(p_lcs, col = cm.colors(256))
-heatmap(p_qgram, col = cm.colors(256))
-heatmap(p_cosine, col = cm.colors(256))
-heatmap(p_jaccard, col = cm.colors(256))
-heatmap(p_jw, col = cm.colors(256)) # this seemes to be the most useful
-heatmap(p_soundex, col = cm.colors(256))
-
-heatmap(p_jw, col = terrain.colors(256)) # this seemes to be the most useful
+# p <- stringdistmatrix(df1$teritorija, df2$teritorija, useNames = TRUE) # uses default method "lv"
+# p_osa <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "osa", useNames = TRUE)
+# p_dl <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "dl", useNames = TRUE)
+# p_hamming <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "hamming", useNames = TRUE)
+# p_lcs <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "lcs", useNames = TRUE)
+# p_qgram <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "qgram", useNames = TRUE)
+# p_cosine <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "cosine", useNames = TRUE)
+# p_jaccard <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "jaccard", useNames = TRUE)
+# p_jw <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "jw", useNames = TRUE)
+# p_soundex <- stringdistmatrix(df1$teritorija, df2$teritorija, method = "soundex", useNames = TRUE)
+# 
+# heatmap(p, col = cm.colors(256))
+# heatmap(p_osa, col = cm.colors(256))
+# heatmap(p_dl, col = cm.colors(256))
+# heatmap(p_hamming, col = cm.colors(256))
+# heatmap(p_lcs, col = cm.colors(256))
+# heatmap(p_qgram, col = cm.colors(256))
+# heatmap(p_cosine, col = cm.colors(256))
+# heatmap(p_jaccard, col = cm.colors(256))
+# heatmap(p_jw, col = cm.colors(256)) # this seemes to be the most useful
+# heatmap(p_soundex, col = cm.colors(256))
+# 
+# heatmap(p_jw, col = terrain.colors(256)) # this seemes to be the most useful
 
 
 # now width removed common strings which are not of interest (to see the contrast better in heatmaps)
@@ -311,23 +314,23 @@ heatmap(p_jw, col = terrain.colors(256)) # this seemes to be the most useful
 
 # df1_rem_str <- as.data.frame(gsub("(Lielbritānijas un Ziemeļīrijas Apvienotā Karaliste)", "", ter_unik$teritorija))
 
-df1_rem_str <- df1
-df1_rem_str$teritorija <- unlist(gsub("(Lielbritānijas un Ziemeļīrijas Apvienotā Karaliste)", "", df1_rem_str$teritorija))
-df1_rem_str$teritorija <- unlist(gsub("Republika", "R.", df1_rem_str$teritorija))
-df1_rem_str$teritorija <- unlist(gsub("Nīderlandes Karaliste", "NK", df1_rem_str$teritorija))
+# df1_rem_str <- df1
+# df1_rem_str$teritorija <- unlist(gsub("(Lielbritānijas un Ziemeļīrijas Apvienotā Karaliste)", "", df1_rem_str$teritorija))
+# df1_rem_str$teritorija <- unlist(gsub("Republika", "R.", df1_rem_str$teritorija))
+# df1_rem_str$teritorija <- unlist(gsub("Nīderlandes Karaliste", "NK", df1_rem_str$teritorija))
 
 
-df2_rem_str <- df1_rem_str
-
-p_rem_jw <- stringdistmatrix(df1_rem_str$teritorija, df2_rem_str$teritorija, method = "jw", useNames = TRUE)
-
-heatmap(p_rem_jw, col = terrain.colors(256)) # this seemes to be the most useful
-
-str(p_jw)
-str(p_rem_jw)
-
-# seems not the best method used. Can some words be ignored?
-p1 <- as.data.frame(stringdistmatrix(df1$teritorija, df2$teritorija, useNames = TRUE))
+# df2_rem_str <- df1_rem_str
+# 
+# p_rem_jw <- stringdistmatrix(df1_rem_str$teritorija, df2_rem_str$teritorija, method = "jw", useNames = TRUE)
+# 
+# heatmap(p_rem_jw, col = terrain.colors(256)) # this seemes to be the most useful
+# 
+# str(p_jw)
+# str(p_rem_jw)
+# 
+# # seems not the best method used. Can some words be ignored?
+# p1 <- as.data.frame(stringdistmatrix(df1$teritorija, df2$teritorija, useNames = TRUE))
 
 
 # Starts/Ends on
@@ -382,8 +385,10 @@ Ends_01.03.2023 <- c()
 # World Bank data to use?
 # https://joenoonan.se/post/country-code-tutorial/
 
-# Using rvest to Scrape an HTML Table https://www.r-bloggers.com/2015/01/using-rvest-to-scrape-an-html-table/
+# Using rvest to Scrape an HTML table https://www.r-bloggers.com/2015/01/using-rvest-to-scrape-an-html-table/
 # Par pasaules valstu un teritoriju nosaukumiem latviešu valodā https://www.vestnesis.lv/op/2018/218.9
+# this table is from the official publication of the names of countries in Latvian as well as width corresponding
+# country codes (ISO). This will help to connect data width other publicly available data.
 
 url5 <- "https://www.vestnesis.lv/op/2018/218.9"
 
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # blacklisted_countries_by_LV
-The purpose of the project to analyse the effects of blacklisting of jurisdictions in Latvia for tax purposes.
 
-The purpose of this project is to analyse the consequences of policy decisions of blacklisting countries for tax purposes. 
+he purpose of this project is to analyse the consequences of policy decisions of blacklisting countries for tax purposes. 
+
+In my Masters Thesis (project https://github.com/Roberts82/Masters_Th_LU_BVEF) where I analysed the effects of double tax treaties on FDI to Latvia, being on the blacklist was used as one of control variables, and no significant effect on FDI to Latvia from a blacklisted country was found. It seems contraintuitive as the purpose of blacklisting is to limit economic cooperation widh the blacklisted countries (usually "blacklisting" happen when a jurisdiction does not cooperate in tax matters (for example does not provide informatation needed to assess tax in the residency country)). Moreover, "blacklisting" makes cooperation width companies from "blacklisted" countries more difficult, as widthholding taxes needed to be paid when dealing width such companies, as well as administrative burden increases. 
+In this project I plan to analyse deeper the consequences of blacklisting of a countries on economic cooperation width the blacklisted countries width Latvia. I plan start width descriptive statistics and then analyse effects of blacklisting on publicly available data that can be used as nexus to estimate economic cooperation of Latvian companies width blacklited companies (for ex., FDI stock, FDI flow, trade, number of companies width capital from blacklisted countries etc.).
+
+The followidn steps planned to be taken:
+*1) scraping data from regulations on the blacklisted countries beginning from 1995 (when Latvia started blacklisting countries which don't cooperate in tax matters) until these days;
+*2) creating dataframes width all the blacklisted countries in all years (starting widh 1995);
+*3) connecting data widh other publicly available data;
+*4) analysing data.
+
+The results are interesting as the program of blacklisting is still in place so this project could help to estimate the effect of this policy.
